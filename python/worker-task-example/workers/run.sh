60130ab5d27b0915587f38ba630862fa976ea103
Pablo Cuadrado <pablocuadrado@gmail.com>
2025-10-15 21:59:12 -0300

    add run script to python example

---
+++ python/worker-task-example/workers/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ "$1" == "-i" ]; then
+  read -s -p "Enter your Conductor Auth Key: " CONDUCTOR_AUTH_KEY
+  echo ""
+  read -s -p "Enter your Conductor Auth Secret: " CONDUCTOR_AUTH_SECRET
+  echo ""
+  read -p "Enter your Conductor Server Url: " CONDUCTOR_SERVER_URL
+  echo ""
+fi
+
+# Load environment variables from .env.local if it exists
+if [ -f ../.env.local ]; then
+  echo "Loading environment variables from .env.local"
+  export $(grep -v '^#' ../.env.local | xargs)
+fi
+
+# Override with command-line arguments if provided and not empty
+# and not "-i"
+if [ ! -z "$1" ] && [ "$1" != "-i" ]; then
+  CONDUCTOR_AUTH_KEY=$1
+fi
+
+if [ ! -z "$2" ]; then
+  CONDUCTOR_AUTH_SECRET=$2
+fi
+
+if [ ! -z "$3" ]; then
+  CONDUCTOR_SERVER_URL=$3
+fi
+
+export CONDUCTOR_AUTH_KEY
+export CONDUCTOR_AUTH_SECRET
+export CONDUCTOR_SERVER_URL
+
+python main.py
